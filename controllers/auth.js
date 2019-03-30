8adefa4b2f5d37859965d8b9f7fa7ca3afe06211
Tanausu Sosa <tanausu_sa@hotmail.com>
2019-03-31 00:16:40 +0700

    Added password reset logic and views and emails when registering or resetting password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,17 @@
-const bcrypt = require('bcryptjs');
-const User = require('../models/user');
+const bcrypt            = require('bcryptjs');
+const User              = require('../models/user');
+const crypto            = require('crypto');
+const nodemailer        = require('nodemailer');
+const sendrigTransport  = require('nodemailer-sendgrid-transport');
+const transporter       = nodemailer.createTransport(sendrigTransport({
+  auth: {
+    api_key: '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'
+  }
+}));
 
 exports.loginPage = (req, res, next) => {
   let message = req.flash('error');
   (message.length > 0) ? message = message[0] : message = null;
-  console.log(message);
   res.render('auth/login', { pageTitle: 'Login to Travel Guides', errorMessage: message });
 }
 
@@ -23,6 +30,115 @@ exports.postLogin = async (req, res, next) => {
     console.log(err);
     next(err);
   }
+}
+
+exports.getSignup = (req, res, next) => {
+  let message = req.flash('error');
+  (message.length > 0) ? message = message[0] : message = null;
+  res.render('auth/signup', { pageTitle: 'Sign Up to Travel Sites', errorMessage: message });
+}
+
+exports.postSignup = async (req, res, next) => {
+  try {
+    const userExist = await User.findOne({ email: req.body.email });
+    if(userExist) {
+      req.flash('error', 'Email already exists.');
+      return res.redirect('/signup');
+    }
+    let { pass1, pass2 } = req.body.password;
+    if(pass1 === pass2) {
+      const user = { email: req.body.email, password: pass1 };
+      user.password = await bcrypt.hash(pass1, 12);
+      await User.create(user);
+      res.redirect('/');
+      return transporter.sendMail({
+        to: req.body.email,
+        from: 'info@travelguides.com',
+        subject: 'Registration Successful',
+        html: '<h1>You are in bro!</h1>'
+      });
+    } else {
+      req.flash('error', 'Passwords do not match.');
+      res.redirect('/signup');
+    }
+  } catch(err) {
+    console.log(err);
+    next(err);
+  }
+}
+
+exports.getResetPassword = (req, res, next) => {
+  let message = req.flash('error');
+  (message.length > 0) ? message = message[0] : message = null;
+  res.render('auth/reset', { pageTitle: 'Reset Password', errorMessage: message });
+}
+
+exports.postResetPassword = async (req, res, next) => {
+  try {
+    crypto.randomBytes(32, async (err, buffer) => {
+      if(err) {
+        return res.redirect('/reset-password');
+      }
+      const token = buffer.toString('hex');
+      const foundUser = await User.findOne({ email: req.body.email });
+      if(!foundUser) {
+        req.flash('error', 'Email not found.');
+        return res.redirect('/reset-password');
+      }
+      foundUser.resetToken = token;
+      foundUser.tokenExpiry = Date.now() + 3600000;
+      await foundUser.save();
+      res.redirect('/');
+      return transporter.sendMail({
+        to: req.body.email,
+        from: 'info@travelguides.com',
+        subject: 'Password Reset',
+        html: `<p>Password reset request: Clik <a href="http://localhost:3000/new-password/${token}">this link</a> to proceed.</p>`
+      });
+    });
+  } catch(err) {
+    console.log(err);
+    next(err);
+  }
+}
+
+exports.getNewPassword = async (req, res, next) => {
+  try {
+    const token = req.params.token;
+    const foundUser = await User.findOne({ resetToken: token, tokenExpiry: { $gt: Date.now() } });
+    let message = req.flash('error');
+    (message.length > 0) ? message = message[0] : message = null;
+    res.render(`auth/newpass`, { pageTitle: 'New Password', errorMessage: message, userId: foundUser._id.toString(), passwordToken: token });
+  } catch(err) {
+    next(err);
+  }
+}
+
+exports.postNewPassword = async (req, res, next) => {
+  try {
+    const { newPassword1, newPassword2 } = req.body.password;
+    const userId = req.body.userId;
+    const passwordToken = req.body.passwordToken;
+    const foundUser = await User.findOne({ resetToken: passwordToken, tokenExpiry: { $gt: Date.now() }, _id: userId });
+    console.log(foundUser);
+    if(!foundUser || newPassword1 !== newPassword2) {
+      return res.redirect('back');
+    }
+    const newPassword = await bcrypt.hash(newPassword1, 12);
+    foundUser.password = newPassword;
+    foundUser.resetToken = null;
+    foundUser.tokenExpiry = null;
+    foundUser.save();
+    res.redirect('/login');
+  } catch(err) {
+    next(err);
+  }
+}
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(() => {
+    res.redirect('/');
+  });
 }
 
 async function validateUser(email, password) {
@@ -44,34 +160,6 @@ function createUserSession(user, req, res) {
   req.session.isLoggedIn = true;
   req.session.user = user;
   return req.session.save(err => {
-    res.redirect('/');
-  });
-}
-
-exports.getSignup = (req, res, next) => {
-  res.render('auth/signup', { pageTitle: 'Sign Up to Travel Sites' });
-}
-
-exports.postSignup = async (req, res, next) => {
-  try {
-    const email = req.body.email;
-    let { pass1, pass2 } = req.body.password;
-    const user = { email: email, password: pass1 };
-    if(pass1 === pass2) {
-      user.password = await bcrypt.hash(pass1, 12);
-      await User.create(user);
-      res.redirect('/');
-    } else {
-      res.redirect('/back');
-    }
-  } catch(err) {
-    console.log(err);
-    next(err);
-  }
-}
-
-exports.postLogout = (req, res, next) => {
-  req.session.destroy(() => {
     res.redirect('/');
   });
 }
--- routes/auth.js
+++ routes/auth.js
@@ -10,6 +10,14 @@ router.route('/signup')
   .get(authController.getSignup)
   .post(authController.postSignup);
 
+router.route('/reset-password')
+  .get(authController.getResetPassword)
+  .post(authController.postResetPassword);
+
+router.route('/new-password/:token')
+  .get(authController.getNewPassword)
+  .post(authController.postNewPassword);
+
 router.post('/logout', authController.postLogout);
 
 module.exports = router;
